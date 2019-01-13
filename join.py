22f4dae043b3d40732c6e242fb0ba1c6d99d84f1
PraJJu <prajwalvrvmp@gmail.com>
2019-01-13 17:59:12 +0530

    Program to print reverse of a string

---
+++ join.py
@@ -0,0 +1,16 @@
+class Join:
+	def join(str):
+		lst1 = []
+		lst = []
+		lst = str.split()
+		lst1 = lst[::-1]	
+		b = ' '
+		print(b.join(lst1))
+	
+	
+	
+	
+	
+inp = input("Enter a statement:")
+a = Join
+a.join(inp)
